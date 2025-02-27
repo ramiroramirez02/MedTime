f8a2256ac745a8e89214551248151c33e4ce505e
Ramiro Ramirez <r_r888@txstate.edu>
2025-02-26 20:26:06 -0600

    Created login feature and profile page

--- medtime/src/pages/Navbar.js
+++ medtime/src/pages/Navbar.js
@@ -10,6 +10,7 @@ function Navbar() {
         <li><Link to="/">Home</Link></li>
         <li><Link to="/signup">Signup</Link></li>
         <li><Link to="/login">Login</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
       </ul>
     </nav>
   );
